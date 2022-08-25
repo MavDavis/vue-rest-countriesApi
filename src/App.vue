48688ebf67213ce9afe54294c2d714463c20dcfc
Mavdavis <ekwesimarvelous@gmail.com>
2022-08-25 01:45:01 -0700

    Done Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
-<router-view/>
+<Navbar/>
+  <router-view/>
 </template>
 
-<style>
+<script>
+import Navbar from './components/navbar.vue';
+export default {
+    components: { Navbar },
 
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,400;0,800;1,600&display=swap');
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: 'Nunito Sans', sans-serif;
+}
+:root{
+  --Dark-Mode-Elements: hsl(209, 23%, 22%);
+--Dark-Mode-Background: hsl(207, 26%, 17%);
+--Light-Mode-Text: hsl(200, 15%, 8%);
+--Light-Mode-Input: hsl(0, 0%, 52%);
+--Light-Mode-Background: hsl(0, 0%, 98%);
+--Dark-Mode-Text: hsl(0, 0%, 100%);
+--Light-Mode-Elements: hsl(0, 0%, 100%);
+  --box-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.2);
+
+}
 </style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,13 +1,57 @@
 <template>
-  
+  <nav >
+    <div class="logo">
+        <h3>Where in the world?</h3>
+    </div>
+    <div class="mode">
+        <div v-if="dark">
+        <i class="fas fa-moon"></i>
+        <span>Dark Mode</span>
+        </div>
+         <div v-else>
+       <i class="fas fa-sun"></i>
+        <span>Light Mode</span>
+        </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-name:"Navbar"
+name:"Navbar",
+data(){
+    return{
+        dark:false,
+    }
 }
+}
+
 </script>
 
 <style>
+nav{
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 5%;
+    margin: 0 0 1rem 0;
+      box-shadow:
+  var(--box-shadow);
 
+  
+}
+
+    .mode span{
+        margin-left: .6rem;
+    }
+
+    @media(max-width:400px){
+        .logo h3, .mode i, .mode span{
+            font-size: 90%;
+        }
+    }
+       @media(max-width:280px){
+        .logo h3, .mode i, .mode span{
+            font-size: 80%;
+        }
+    }
 </style>
